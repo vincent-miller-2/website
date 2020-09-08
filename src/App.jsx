ece4cc4d003b11a7e3398c8c0e1059e2f92585ed
Vincent Miller <vincent.miller@workstate.com>
2020-09-08 12:56:22 -0400

    Fixing font size

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-plusplus */
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import SideBar from './components/Sidebar';
